653165719a090acfa42342b50cc5ac8284961727
root <root@instancia2.us-central1-b.c.coltrekking-app.internal>
2023-05-19 18:48:59 +0000

    Revert "Updating Firebase Config."
    This reverts commit ee2f93abf9fc2f7f026f706ab2a39c5ff649c365.

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAYwLIkAUkXJvUeNqf-Uy0YpdoqAkSDIY4",
-  authDomain: "coltrekking.firebaseapp.com",
-  projectId: "coltrekking",
-  storageBucket: "coltrekking.appspot.com",
-  messagingSenderId: "1082334786056",
-  appId: "1:1082334786056:web:9884aab1e74c8da305d7bd"
+// Inicializa Firebase
+var config = {
+	apiKey: "AIzaSyDWuZXZioUNujSBLLOtH1tRF8IWCIqMH4g",
+	authDomain: "coltrekking-b81da.firebaseapp.com",
+	databaseURL: "https://coltrekking-b81da.firebaseio.com",
+	storageBucket: "coltrekking-b81da.appspot.com",
+	messagingSenderId: "1020694218506"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(config);
