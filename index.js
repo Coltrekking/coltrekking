b9058df7741c147e71ecf2b31584ac1429965e3e
root <root@instancia2.us-central1-b.c.coltrekking-app.internal>
2023-05-21 23:46:15 +0000

    Corrigindo porta do serviço de 3000 para 80.

--- index.js
+++ index.js
@@ -962,7 +962,7 @@ function montaRanking(ano, connection, callback) {
 }
 
 /*************************INICIA SERVIDOR*****************************/
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 
 app.listen(port, function () {
 	//console.log("Ouvindo na porta " + port);
