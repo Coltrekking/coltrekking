c962be7218171e3ad0c0cef41174e854ec754f05
Inaue <cafe.ina@outlook.com>
2023-05-23 20:27:02 +0000

    Reestruturando o Firebase.

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,19 @@
-// Inicializa Firebase
-var config = {
-	apiKey: "AIzaSyDWuZXZioUNujSBLLOtH1tRF8IWCIqMH4g",
-	authDomain: "coltrekking-b81da.firebaseapp.com",
-	databaseURL: "https://coltrekking-b81da.firebaseio.com",
-	storageBucket: "coltrekking-b81da.appspot.com",
-	messagingSenderId: "1020694218506"
-};
-firebase.initializeApp(config);
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
+
+const firebaseConfig    =
+{
+    apiKey: "AIzaSyAYwLIkAUkXJvUeNqf-Uy0YpdoqAkSDIY4",
+    authDomain: "coltrekking.firebaseapp.com",
+    projectId: "coltrekking",
+    storageBucket: "coltrekking.appspot.com",
+    messagingSenderId: "1082334786056",
+    appId: "1:1082334786056:web:9884aab1e74c8da305d7bd"
+}
+
+export const auth   = {}
+auth.app            = initializeApp(firebaseConfig)
+auth.servico        = getAuth(auth.app)
+auth.provedorGmail  = new GoogleAuthProvider()
+auth.logar          = signInWithPopup
+auth.mudouEstado    = onAuthStateChanged
