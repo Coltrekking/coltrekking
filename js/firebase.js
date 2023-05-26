2b8719bbc5b1c03a86174fed5029ab056c83c1ec
Inaue <cafe.ina@outlook.com>
2023-05-26 14:53:48 +0000

    Isolando recursos gerais do firebase.

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
 import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
 
-const firebaseConfig    =
+const firebase          = {}
+firebase.config         =
 {
     apiKey: "AIzaSyAYwLIkAUkXJvUeNqf-Uy0YpdoqAkSDIY4",
     authDomain: "coltrekking.firebaseapp.com",
@@ -10,9 +11,10 @@ const firebaseConfig    =
     messagingSenderId: "1082334786056",
     appId: "1:1082334786056:web:9884aab1e74c8da305d7bd"
 }
+firebase.app            = initializeApp(firebase.config)
 
 export const auth       = {}
-auth.app                = initializeApp(firebaseConfig)
+auth.app                = firebase.app
 auth.servico            = getAuth(auth.app)
 auth.provedorGmail      = new GoogleAuthProvider()
 auth.signInWithPopup    = signInWithPopup
