1d495691866f8332d96f124f2fa8f489d7984341
Inaue <100319268+Inaue@users.noreply.github.com>
2023-06-26 17:32:19 +0000

    Removendo pacotes nao utilizados no index.

--- index.mjs
+++ index.mjs
@@ -1,7 +1,4 @@
 /********************************SETUP**********************************/
-import * as http	from 'http'
-import * as fs		from 'fs'
-import * as url		from 'url'
 import * as path	from 'path'
 import express		from 'express'
 import bodyParser	from 'body-parser'
