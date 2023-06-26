1281bcd2407ab7ac30dddc37707f10c7df1da02f
Inaue <100319268+Inaue@users.noreply.github.com>
2023-06-26 17:31:29 +0000

    Padronizando chamada de funcoes, calbacks e rotas.

--- index.mjs
+++ index.mjs
@@ -35,23 +35,18 @@ app.use(express.static('./', {
 }));
 
 /*********************************PAGINAS********************************/
+
 var index = '/html/index.html';
 var login = '/html/login/index.html';
 
-/******************************REQUISICOES*******************************/
+/*********************************FUNCOES********************************/
 
-const execute	= (req, res, func) =>
-{
-	if (req.session.usuarioLogado.ID)
-	{
-		db.handleDatabase(req, res, (req, res, connection) =>
-		{
-			func(req, req.body, connection, (status) => res.send(status));
-		});
-	}
-	else
-		res.send(false);
-}
+//*****Retorno para acoes no DB*****//
+
+const enviaEstado	= (res, status)	=> res.send(status)
+const enviaLinhas	= (res, rows)	=> res.send(rows)
+
+/******************************REQUISICOES*******************************/
 
 //*****Carrega pagina inicial*****//
 app.get("/", function (req, res) {
@@ -60,42 +55,88 @@ app.get("/", function (req, res) {
 });
 
 /* EVENTOS */
-app.post("/criar-evento",			(req, res) => execute(req, res, evento.criarEventoDB));
-app.post("/editar-evento",			(req, res) => execute(req, res, evento.editarEventoDB));
-app.post("/confirmar-evento",		(req, res) => execute(req, res, evento.confirmarEventoDB));
-app.post("/cancelar-evento",		(req, res) => execute(req, res, evento.cancelarEventoDB));
-app.post("/cadastrar-pontuacao",	(req, res) => execute(req, res, evento.cadastrarPontucaoDB));
-app.post("/finalizar-evento",		(req, res) => execute(req, res, evento.finalizarEventoDB));
-app.post("/excluir-evento",			(req, res) => execute(req, res, evento.excluirEventoDB));
+app.post("/criar-evento",			(req, res) => db.handleDatabase(req, res, evento.criarEventoDB,			enviaEstado))
+app.post("/editar-evento",			(req, res) => db.handleDatabase(req, res, evento.editarEventoDB,		enviaEstado))
+app.post("/confirmar-evento",		(req, res) => db.handleDatabase(req, res, evento.confirmarEventoDB,		enviaEstado))
+app.post("/cancelar-evento",		(req, res) => db.handleDatabase(req, res, evento.cancelarEventoDB,		enviaEstado))
+app.post("/cadastrar-pontuacao",	(req, res) => db.handleDatabase(req, res, evento.cadastrarPontucaoDB,	enviaEstado))
+app.post("/finalizar-evento",		(req, res) => db.handleDatabase(req, res, evento.finalizarEventoDB, 	enviaEstado))
+app.post("/excluir-evento",			(req, res) => db.handleDatabase(req, res, evento.excluirEventoDB, 		enviaEstado))
 
 /* POSTAGENS */
+/*
 app.post("/criar-postagem",			(req, res) => execute(req, res, postagem.criarPostagemDB));
 app.post("/editar-info",			(req, res) => execute(req, res, postagem.editarInfoDB));
 app.post("/get-postagem",			(req, res) => execute(req, res, postagem.getPostagemDB));
 app.post("/excluir-postagem",		(req, res) => execute(req, res, postagem.excluirPostagemDB));
-
+*/
 /* LISTA NEGRA */
+/*
 app.post("/adicionar-lista-negra",	(req, res) => execute(req, res, lstNeg.adicionarListaNegraDB));
 app.post("/remover-lista-negra",	(req, res) => execute(req, res, lstNeg.removerListaNegraDB));
 app.post("/excluir-usuario",		(req, res) => execute(req, res, evento.excluirUsuarioDB));
-
+*/
 //*****Posta usuario logado*****//
-app.post("/post-user", function (req, res) {
-	if (!req.body.ID || req.body.ID.length < 5) {
+
+app.post("/post-user", (req, res) =>
+{
+	if (!req.body.ID || req.body.ID.length < 5)
+	{
 		res.status(500);
 		res.send("Problema com o firebase");
-	} else {
+	}
+	else
+	{
+		req.session.usuarioLogado = req.body;
+
+		db.handleDatabase(req, res, user.addDB,	(res, status) =>
+		{
+			if (status)
+			{
+				req.session.loginSucesso = true;
+
+				db.handleDatabase(req, res, user.pegaInfoUsuarioLogado,	(res, status) =>
+				{
+						if (status)
+
+							res.send("/main-page")
+						else
+						{
+							res.status(500);
+							res.send("Algo inesperado aconteceu");
+						}
+				})
+			}
+			else
+			{
+				res.status(500);
+				res.send("Problema com o firebase");
+			}
+		})
+	}
+})
+
+/*
+app.post("/post-user", function (req, res)
+{
+	if (!req.body.ID || req.body.ID.length < 5)
+	{
+		res.status(500);
+		res.send("Problema com o firebase");
+	}
+	else
+	{
 		req.session.usuarioLogado = req.body;
 
 		//Adiciona usuario ao DB
 		db.handleDatabase(req, res, function (req, res, connection) {
-			user.addDB(req, connection, function (status) {
+			user.addDB(req, res, connection, function (status) {
 				if (status) {
 					req.session.loginSucesso = true;
 
 					db.handleDatabase(req, res, function (req, res, connection) {
 						//Pega info como fatork, posicao, etc
-						user.pegaInfoUsuarioLogado(req, connection, function (status) {
+						user.pegaInfoUsuarioLogado(req, res, connection, function (status) {
 							if (status) {
 								//Depois de fazer login, manda pagina a ser redirecionado
 								res.send("/main-page");
@@ -111,9 +152,9 @@ app.post("/post-user", function (req, res) {
 				}
 			});
 		});
-
 	}
 });
+*/
 
 //*****Redireciona para pagina principal*****//
 app.get("/main-page", function (req, res) {
@@ -131,68 +172,12 @@ app.get("/get-user", function (req, res) {
 	res.json(req.session.usuarioLogado);
 });
 
-//******Get informacoes iniciais*****//
-app.get("/informacoesiniciais", function (req, res) {
-	db.handleDatabase(req, res, function (req, res, connection) {
-		getInformacoesiniciais(connection, function (rows) {
-			res.send(rows);
-		});
-	});
-});
+app.get("/informacoesiniciais",		(req, res) => db.handleDatabase(req, res, getInformacoesiniciais,	enviaLinhas))
+app.get("/eventos",					(req, res) => db.handleDatabase(req, res, evento.getEventos,		enviaLinhas))
+app.post("/confirmados",			(req, res) => db.handleDatabase(req, res, getConfirmados,			enviaLinhas))
+app.post("/confirmados-por-mim",	(req, res) => db.handleDatabase(req, res, getConfirmadosPorMim,		enviaLinhas))
+app.post("/ranking",				(req, res) => db.handleDatabase(req, res, evento.montaRanking,		enviaLinhas))
 
-//******Get Eventos*****//
-app.get("/eventos", function (req, res) {
-	if (!req.session.usuarioLogado.ID) {
-		res.send(false);
-	} else {
-		db.handleDatabase(req, res, function (req, res, connection) {
-			evento.getEventos(connection, function (rows) {
-				res.send(rows);
-			});
-		});
-	}
-});
-
-//*****Post Confirmados*****//
-app.post("/confirmados", function (req, res) {
-	if (!req.session.usuarioLogado.ID) {
-		res.send(false);
-	} else {
-		db.handleDatabase(req, res, function (req, res, connection) {
-			getConfirmados(req.body.IDEvento, connection, function (rows) {
-				res.send(rows);
-			});
-		});
-	}
-});
-
-//*****Post Confirmados por Mim*****//
-app.post("/confirmados-por-mim", function (req, res) {
-	if (!req.session.usuarioLogado.ID) {
-		res.send(false);
-	} else {
-		db.handleDatabase(req, res, function (req, res, connection) {
-			getConfirmadosPorMim(req.body.usuarioID, connection, function (rows) {
-				res.send(rows);
-			});
-		});
-	}
-});
-
-//*****Ranking*****//
-app.post("/ranking", function (req, res) {
-	if (!req.session.usuarioLogado.ID) {
-		res.send(false);
-	} else {
-		db.handleDatabase(req, res, function (req, res, connection) {
-			evento.montaRanking(req.body.ano, connection, function callback(rows) {
-				res.send(rows);
-			});
-		});
-	}
-});
-
-//*****Log Out*****//
 app.get("/logout", function (req, res) {
 	//Usuario deslogado
 	req.session.loginSucesso = false;
@@ -204,45 +189,52 @@ app.get("/logout", function (req, res) {
 
 /***************************BANCO DE DADOS*****************************/
 //*****Get Informacoes Iniciais*****//
-function getInformacoesiniciais(connection, callback) {
+function getInformacoesiniciais(req, res, connection, callback)
+{
 	connection.query('SELECT Texto, ComoParticipar, Calendario, Regras FROM postagem WHERE ID = 1', function (err, rows, fields) {
 		connection.release();
 		if (!err) {
-			callback(rows);
+			callback(res, rows);
 		} else {
-			callback(false);
+			callback(res, false);
 		}
 	});
 }
 
 //*****Get Confirmados*****//
-function getConfirmados(data, connection, callback) {
+function getConfirmados(req, res, connection, callback)
+{
+	let data	= req.body.IDEvento
+
 	//Get os IDs dos confirmados com INNER JOIN
 	connection.query('SELECT ID, Nome, FatorK, rg, `pessoa-evento`.ListaEspera, `pessoa-evento`.IDEvento, `pessoa-evento`.Colocacao, `pessoa-evento`.DataHoraInscricao, `pessoa-evento`.DataInscricao, `pessoa-evento`.listaNegraEvento FROM `pessoa` INNER JOIN `pessoa-evento` ON pessoa.ID = `pessoa-evento`.IDPessoa WHERE `pessoa-evento`.IDEvento = ? ORDER BY `pessoa-evento`.DataHoraInscricao, `pessoa-evento`.Colocacao', data, function (err, rows, fields) {
 		connection.release();
 
 		if (!err) {
-			callback(rows);
+			callback(res, rows);
 		} else {
-			callback(false);
+			callback(res, false);
 		}
 	});
 }
 
 //*****Get Confirmados Por Mim*****//
-function getConfirmadosPorMim(data, connection, callback) {
+function getConfirmadosPorMim(req, res, connection, callback)
+{
+	let data	= req.body.usuarioID
+
 	connection.query('SELECT ID, Nome FROM `evento` INNER JOIN `pessoa-evento` ON evento.ID = `pessoa-evento`.IDEvento WHERE `pessoa-evento`.IDPessoa = ? ORDER BY ID DESC', data, function (err, rows, fields) {
 		connection.release();
 
 		if (!err) {
-			callback(rows);
+			callback(res, rows);
 		} else {
-			callback(false);
+			callback(res, false);
 		}
 	});
 }
 
 /*************************INICIA SERVIDOR*****************************/
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 
 app.listen(port, () => {} );
