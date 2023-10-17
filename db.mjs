03badb967fecb4ba3aeb6a995289896137e0ef90
Inaue <100319268+Inaue@users.noreply.github.com>
2023-10-17 19:53:13 +0000

    Adicionando Axios para conectar com novo db.

--- db.mjs
+++ db.mjs
@@ -1,4 +1,8 @@
 import * as mysql	from 'mysql'
+import axios        from 'axios'
+
+const url_db     = 'https://script.google.com/macros/s/AKfycbwyM12VBUzdDK1ktnwykHN4UKpFoIWxM8O1FR9qBvJ30S_DeMorJ5Oz5Fi7A-oM4ULEnQ/exec'
+const conexao_db = axios.create({ baseURL: url_db })
 
 const pool  = mysql.createPool
 ({
@@ -15,22 +19,21 @@ const executa	= (req, res, action, callback) =>
 {
 	if(req.session.usuarioLogado.ID)
 	{
-		pool.getConnection(function (err, connection)
+		/*
+		if (err)
 		{
-			if (err)
-			{
-				res.json({ "code": 100, "status": "Error in connection database" });
-				return false;
-			}
-
-			action(req, res, connection, callback);
-
-			connection.on('error', function (err)
-			{
-				res.json({"code": 100, "status": "Error in query"});
-				return false;
-			});
+			res.json({ "code": 100, "status": "Error in connection database" });
+			return false;
+		}
+	   */
+		action(req, res, conexao_db, callback);
+/*
+		connection.on('error', function (err)
+		{
+			res.json({"code": 100, "status": "Error in query"});
+			return false;
 		});
+*/
 	}
 	else
 		res.send(false)
